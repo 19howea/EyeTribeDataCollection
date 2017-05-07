cf6606f44baeb3e98688f24b928715eb4b36f17b
Unknown <19howea@asij.ac.jp>
2017-05-07 16:31:48 +0900

    Update to plot multiple files.

--- R_Analysis/TimeDistributionOnLookingAtPassageAndQuestion.R
+++ R_Analysis/TimeDistributionOnLookingAtPassageAndQuestion.R
@@ -2,26 +2,36 @@
 script.dir <- dirname(sys.frame(1)$ofile)
 setwd(script.dir)
 
-# Read csv file
-df = read.csv("../EyeGazeData/20170429_160122_Andrew_Howe_GazeData.csv", header = FALSE)
-colnames(df) = c("timeStamp", "xCordinate", "yCordinate")
+# Prepare the path for files
+base.folder<- paste(script.dir,"/../EyeGazeData/",sep="")
+file.list <- list.files(base.folder)
+path <- paste(base.folder, file.list, sep ="")
 
-remove_list = c()
-for (i in 1:nrow(df)){
-  if ((df$xCordinate[i] == 0 && df$yCordinate[i] == 0) ||
-      df$xCordinate[i] < 0 || df$yCordinate[i] < 0) {
-    remove_list = c(remove_list, i)
+for (j in 1:length(path)) {
+  # Read all CSV file
+  df = read.csv(path[j], header = FALSE)
+  colnames(df) = c("timeStamp", "xCordinate", "yCordinate")
+  
+  remove_list = c()
+  for (i in 1:nrow(df)){
+    if ((df$xCordinate[i] == 0 && df$yCordinate[i] == 0) ||
+        df$xCordinate[i] < 0 || df$yCordinate[i] < 0) {
+      remove_list = c(remove_list, i)
+    }
   }
+  df.cleaned = df[- remove_list, ]
+  
+  df.passage <- subset(df.cleaned, xCordinate <= 1920/2)
+  df.question <- subset(df.cleaned, xCordinate > 1920/2)
+  
+  slices <- c(nrow(df.passage), nrow(df.question))
+  labels <- c("Passage", "Question")
+  pct <- round(slices/sum(slices)*100)
+  labels <- paste(labels, " ", pct, "%", sep = "")
+  
+  pie(slices, labels = labels, 
+      main = paste("Time spent distribution\n",file.list[j]))
+  
+  readline(prompt = "Please type Enter to continue...")
 }
 
-df.cleaned = df[- remove_list, ]
-
-df.passage <- subset(df.cleaned, xCordinate <= 1920/2)
-df.question <- subset(df.cleaned, xCordinate > 1920/2)
-
-slices <- c(nrow(df.passage), nrow(df.question))
-labels <- c("Passage", "Question")
-pct <- round(slices/sum(slices)*100)
-labels <- paste(labels, " ", pct, "%", sep = "")
-
-pie(slices, labels = labels, main = "Time Distribution")
